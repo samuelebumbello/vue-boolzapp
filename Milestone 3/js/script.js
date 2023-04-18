ed5707c719244993860b2e180e1add38b729db27
samuelebumbello <samuelebumbello17@gmail.com>
2023-04-18 17:45:19 +0200

    [add] keyup.enter end msgReceived user 'Ok'

--- Milestone 3/js/script.js
+++ Milestone 3/js/script.js
@@ -12,6 +12,24 @@ createApp({
         selectedProfile(i){
             this.counter = i
             console.log(this.selectedProfile);
+        },
+        msgEnter(){
+            const newMsg = {
+                message:this.msgInvio,
+                status: "sent"
+            }
+            this.msgInvio = '';
+            this.contacts[this.counter].messages.push(newMsg)
+            this.msgReceived()
+        },
+        msgReceived(){
+            setTimeout(() => {
+                const answer = {
+                    message: 'Ok',
+                    status: 'received'
+                }
+                this.contacts[this.counter].messages.push(answer)
+            },1000)
         }
     }
 
