4cae4903fc3bb4fc05d8f0c85e389009771df016
samuelebumbello <samuelebumbello17@gmail.com>
2023-04-19 20:52:46 +0200

    [add] Milestone 4

---
+++ Milestone 4/js/script.js
@@ -0,0 +1,41 @@
+const { createApp } = Vue;
+import contacts from "./db.js";
+
+createApp({
+    data(){
+        return{
+            contacts,
+            counter : 0
+        }
+    },
+    methods:{
+        selectedProfile(i){
+            this.counter = i
+            console.log(this.selectedProfile);
+        },
+        msgEnter(){
+            if(this.msgInvio.length === 0){
+
+            }else{
+                const newMsg = {
+                    message:this.msgInvio,
+                    status: "sent"
+                }
+                this.msgInvio = '';
+                this.contacts[this.counter].messages.push(newMsg)
+                this.msgReceived()
+
+            }
+        },
+        msgReceived(){
+            setTimeout(() => {
+                const answer = {
+                    message: 'Ok',
+                    status: 'received'
+                }
+                this.contacts[this.counter].messages.push(answer)
+            },1000)
+        }
+    }
+
+}).mount('#app')
